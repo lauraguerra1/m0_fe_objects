c14863019c206fa4cd09c21305913affe66566dc
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-10 13:04:10 -0500

    Clean up syntax

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,61 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48
+};
+  /*
+  Actions: 
+  1. Move the apples key to a new line.
+  2. Move the closing curly bracket to a new line.
+  3. Add a semicolon to the end of the object, after the closing curly bracket. 
 
-var person = { height: "6 ft", weight: "160 lbs" }
+  Reason for all actions: To follow conventions and best practices. 
+  */
 
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
 }
+  /*
+  Actions: 
+  1. Move each key to a new line.
+  2. Move the closing curly bracket to a new line.
+  3. Add a semicolon to the end of the object, after the closing curly bracket. 
+
+  Reason for all actions: To follow conventions and best practices. 
+  */
+
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+};
+ /*
+  Actions: 
+  1. Move each key to a new line.
+  2. Remove blank lines between keys. 
+  3. Use tab to move the comments key in line with the rest of the code block.
+  4. Move the closing curly bracket to a new line.
+  5. Add a semicolon to the end of the object, after the closing curly bracket. 
+
+  Reason for all actions: To follow conventions and best practices. 
+  */
+
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+};
+
+/*
+Actions: 
+1. Move opening curly bracket to the line in which the object variable is declared. 
+2. Move code block back so that it is nested inside the object by only one tab space.
+3. Add a semicolon to the end of the object, after the closing curly bracket. 
+
+Reason for all actions: To follow conventions and best practices. 
+*/
